1af94249abe13fe940e2cdfe52156a3b490c4acd
sunny <lxu@dotfashion.cn>
2018-03-16 01:26:26 +0800

    mongodb add user

--- model/db.js
+++ model/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/blog');
+mongoose.connect(`mongodb://${process.env.USER}:${process.env.PWD}@${process.env.HOST}:${process.env.MONGO_PORT}/${process.env.DBNAME}`);
 
 module.exports = mongoose;
